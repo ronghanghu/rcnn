0f6838d399e645a724c3b95c1f96e5c016e91291
Ronghang Hu <huronghang@hotmail.com>
2014-07-24 22:13:53 -0700

    window data

--- finetuning/cache_voc_2007.m
+++ finetuning/cache_voc_2007.m
@@ -1,2 +1,8 @@
-net_proto_file = 'net_spp_lsda/spp_lsda_5_scale.prototxt';
-net_binary_file = 'net_spp_lsda/spp_lsda.bin';
+net_proto_file = 'external/caffe/examples/spp-rcnn-feat-cache/spp_rcnn_output_spp5.prototxt';
+net_binary_file = 'external/caffe/examples/spp-rcnn-feat-cache/spp_rcnn_output_spp5.bin';
+gpu_id = 1;
+
+imdb_trainval = imdb_from_voc('datasets/VOCdevkit2007', 'trainval', '2007');
+output_dir = '/x/ronghang/voc2007_training';
+
+rcnn_make_spp_cache(imdb_trainval, output_dir, net_proto_file, net_binary_file, gpu_id);
