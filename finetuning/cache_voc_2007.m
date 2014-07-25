b89af530c6d85adc55a886c151e15285a36a2278
Ronghang Hu <huronghang@hotmail.com>
2014-07-24 21:29:40 -0700

    window data

---
+++ finetuning/cache_voc_2007.m
@@ -0,0 +1,2 @@
+net_proto_file = 'net_spp_lsda/spp_lsda_5_scale.prototxt';
+net_binary_file = 'net_spp_lsda/spp_lsda.bin';
--- finetuning/rcnn_make_spp_cache.m
+++ finetuning/rcnn_make_spp_cache.m
@@ -1,4 +1,4 @@
-function rcnn_make_spp_cache(imdb, out_dir)
+function rcnn_make_spp_cache(imdb, out_dir, net_proto_file, net_binary_file, gpu_id)
 % rcnn_make_window_file(imdb, out_dir)
 %   Makes a window file that can be used by the caffe WindowDataLayer 
 %   for finetuning.
@@ -24,24 +24,39 @@ function rcnn_make_spp_cache(imdb, out_dir)
 % this file (or any portion of it) in your project.
 % ---------------------------------------------------------
 
+load rcnn_model_spp.mat;
 roidb = imdb.roidb_func(imdb);
 
+% initialize caffe
+caffe('init', net_proto_file, net_binary_file);
+caffe('set_device', gpu_id);
+caffe('set_phase_test');
+caffe('set_mode_gpu');
 
 for i = 1:length(imdb.image_ids)
-  tic_toc_print('make window file: %d/%d\n', i, length(imdb.image_ids));
+  fprintf('caching feature: %d/%d\n', i, length(imdb.image_ids));
   img_path = imdb.image_at(i);
   roi = roidb.rois(i);
   num_boxes = size(roi.boxes, 1);
-
+  
+  % extract features
+  tic
+  im = imread(img_path);
+  % the roi.boxes are [x1 y1 x2 y2], 1-indexed
+  feat = spp_features_5_scale(im, roi.boxes, rcnn_model);
+  toc
+  
+  % store features to disk
+  im_id = i - 1;
+  mkdir(fullfile(out_dir, num2str(im_id)));
   for j = 1:num_boxes
-    [ov, label] = max(roi.overlap(j,:));
-    % zero overlap => label = 0 (background)
-    if ov < 1e-5
-      label = 0;
-      ov = 0;
-    end
-    bbox = roi.boxes(j,:)-1;
-    keyboard
+    bbox = roi.boxes(j,:)-1; % bbox is [x1 y1 x2 y2], 0-indexed
+    % file name: $(im_id)/$x1_$y1_$x2_$y2.spp5feat
+    file_name = sprintf('%d_%d_%d_%d.spp5feat', bbox(1), bbox(2), bbox(3), bbox(4));
+    file_path = fullfile(out_dir, num2str(im_id), file_name);
+    fid = fopen(file_path, 'w');
+    fwrite(fid, feat(j, :)', 'single');
+    fclose(fid);
   end
 end
 
