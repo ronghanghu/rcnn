3930432c3aa0733d0018ef32c9aaf036aba09624
Ronghang Hu <huronghang@hotmail.com>
2014-07-26 16:40:16 -0700

    changing feature cache link

--- finetuning/rcnn_make_spp_cache.m
+++ finetuning/rcnn_make_spp_cache.m
@@ -1,4 +1,4 @@
-function rcnn_make_spp_cache(imdb, out_dir, net_proto_file, net_binary_file, gpu_id)
+function rcnn_make_spp_cache(imdb, out_dir, net_proto_file, net_binary_file, save_as_mat, gpu_id)
 % rcnn_make_window_file(imdb, out_dir)
 %   Makes a window file that can be used by the caffe WindowDataLayer 
 %   for finetuning.
@@ -34,26 +34,39 @@ caffe('set_phase_test');
 caffe('set_mode_gpu');
 
 for i = 1:length(imdb.image_ids)
+  th0 = tic();
   fprintf('caching feature: %d/%d\n', i, length(imdb.image_ids));
+  
+  % extract features
+  th1 = tic()
   img_path = imdb.image_at(i);
   roi = roidb.rois(i);
   num_boxes = size(roi.boxes, 1);
-  
-  % extract features
   im = imread(img_path);
   % the roi.boxes are [x1 y1 x2 y2], 1-indexed
   feat = spp_features_5_scale(im, roi.boxes, rcnn_model_spp);
+  fprintf('[Extracting feature: %f]\n', toc(th1));
   
   % store features to disk
+  th2 = tic();
   im_id = i - 1;
   mkdir(fullfile(out_dir, num2str(im_id)));
   for j = 1:num_boxes
     bbox = roi.boxes(j,:)-1; % bbox is [x1 y1 x2 y2], 0-indexed
     % file name: $(im_id)/$x1_$y1_$x2_$y2.spp5feat
-    file_name = sprintf('%d_%d_%d_%d.spp5feat', bbox(1), bbox(2), bbox(3), bbox(4));
-    file_path = fullfile(out_dir, num2str(im_id), file_name);
-    fid = fopen(file_path, 'w');
-    fwrite(fid, feat(j, :)', 'single');
-    fclose(fid);
+    if save_as_mat
+      file_name = sprintf('%d_%d_%d_%d.mat', bbox(1), bbox(2), bbox(3), bbox(4));
+      file_path = fullfile(out_dir, num2str(im_id), file_name);
+      spp5 = feat(j, :)';
+      save(file_path, 'spp5');
+    else
+      file_name = sprintf('%d_%d_%d_%d.spp5feat', bbox(1), bbox(2), bbox(3), bbox(4));
+      file_path = fullfile(out_dir, num2str(im_id), file_name);
+      fid = fopen(file_path, 'w');
+      fwrite(fid, feat(j, :)', 'single');
+      fclose(fid);
+    end
   end
+  fprintf('[Saving feature: %f]\n', toc(th2));
+  fprintf('[Total: %f]\n', toc(th0));
 end
