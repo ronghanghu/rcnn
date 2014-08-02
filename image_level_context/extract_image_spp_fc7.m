2a936cce832a9e03601a4f864e50a5d2879d9052
Ronghang Hu <huronghang@hotmail.com>
2014-08-01 21:02:05 -0700

    clean up repo

--- image_level_context/extract_image_spp_fc7.m
+++
@@ -1,8 +0,0 @@
-function feat = extract_image_spp_fc7(im, rcnn_model)
-
-[h, w, ~] = size(im);
-
-whole_image = [1, 1, w, h]; % [x1 y1 x2 y2], 1-indexed
-feat = spp_features_1_scale_(im, whole_image, rcnn_model);
-
-end
