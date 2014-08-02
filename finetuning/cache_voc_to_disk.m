ee48be1a2ca44f637b658b7cf818f2804fe673f8
Ronghang Hu <huronghang@hotmail.com>
2014-08-02 12:08:18 -0700

    update cache_voc_to_disk

--- finetuning/cache_voc_to_disk.m
+++ finetuning/cache_voc_to_disk.m
@@ -3,14 +3,18 @@ net_binary_file = './data/caffe_nets/spp_zf_iter_315000';
 save_as_mat = false;
 gpu_id = 0;
 
-% cache trainval
+% make imdb
 imdb_trainval = imdb_from_voc('datasets/VOCdevkit2007', 'trainval', '2007');
-output_dir = '/x/ronghang/voc2007/trainval';
+imdb_test = imdb_from_voc('datasets/VOCdevkit2007', 'test', '2007');
+
+% make window data file
 rcnn_make_window_file(imdb_trainval, 'external/caffe/examples/pascal-finetuning-voc2007');
+rcnn_make_window_file(imdb_test, 'external/caffe/examples/pascal-finetuning-voc2007');
+
+% cache trainval
+output_dir = '/x/ronghang/voc2007/trainval';
 rcnn_make_spp_cache(imdb_trainval, output_dir, net_proto_file, net_binary_file, save_as_mat, gpu_id);
 
 % cache test
-% imdb_test = imdb_from_voc('datasets/VOCdevkit2007', 'test', '2007');
-% output_dir = '/x/ronghang/voc2007/test';
-% rcnn_make_window_file(imdb_test, 'external/caffe/examples/pascal-finetuning-voc2007');
-% rcnn_make_spp_cache(imdb_test, output_dir, net_proto_file, net_binary_file, save_as_mat, gpu_id);
+output_dir = '/x/ronghang/voc2007/test';
+rcnn_make_spp_cache(imdb_test, output_dir, net_proto_file, net_binary_file, save_as_mat, gpu_id);
