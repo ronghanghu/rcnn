cc23a4b641809024ea8c946bf8132ff688006976
Ronghang Hu <huronghang@hotmail.com>
2014-08-06 17:41:02 -0700

    new batches to cache spp5 features to disk

--- finetuning/cache_voc_to_disk.m
+++ finetuning/cache_voc_to_disk.m
@@ -4,25 +4,27 @@ gpu_id = 0;
 caffe('set_mode_gpu');
 caffe('set_device', gpu_id);
 
-spp_window_data_param.spp5_dim = 12800;
+% same type of parameter as SPPWindowDataLayer
+spp_window_data_param.feat_dim = 12800;
 spp_window_data_param.batch_per_file = 20;
 spp_window_data_param.batch_size = 128;
 spp_window_data_param.fg_fraction = 0.25;
-spp_window_data_param.fg_overlap_max = 1000; % large than 1
+spp_window_data_param.fg_overlap_max = 1.05; % larger than 1
 spp_window_data_param.fg_overlap_min = 0.5;
 spp_window_data_param.bg_overlap_max = 0.5;
 spp_window_data_param.bg_overlap_min = 0.1;
+spp_window_data_param.extension = '.feat_cache';
 
 % make imdb
 imdb_trainval = imdb_from_voc('datasets/VOCdevkit2007', 'trainval', '2007');
 imdb_test = imdb_from_voc('datasets/VOCdevkit2007', 'test', '2007');
 
 % cache trainval
-output_dir = '/x/ronghang/voc2007/trainval';
-rcnn_make_spp_cache(imdb_trainval, output_dir, ...
+spp_window_data_param.cache_dir = '/x/ronghang/voc2007/trainval';
+rcnn_make_spp_cache(imdb_trainval, ...
     net_proto_file, net_binary_file, spp_window_data_param);
 
 % cache test
-output_dir = '/x/ronghang/voc2007/test';
-rcnn_make_spp_cache(imdb_test, output_dir, ...
+spp_window_data_param.cache_dir = '/x/ronghang/voc2007/test';
+rcnn_make_spp_cache(imdb_test, ...
     net_proto_file, net_binary_file, spp_window_data_param);
--- finetuning/rcnn_make_spp_cache.m
+++ finetuning/rcnn_make_spp_cache.m
@@ -1,4 +1,4 @@
-function rcnn_make_spp_cache(imdb, out_dir, net_proto_file, net_binary_file, ...
+function rcnn_make_spp_cache(imdb, net_proto_file, net_binary_file, ...
     spp_window_data_param)
 % rcnn_make_window_file(imdb, out_dir)
 %   Makes a window file that can be used by the caffe WindowDataLayer
@@ -25,7 +25,7 @@ function rcnn_make_spp_cache(imdb, out_dir, net_proto_file, net_binary_file, ...
 % this file (or any portion of it) in your project.
 % ---------------------------------------------------------
 
-spp5_dim = spp_window_data_param.spp5_dim;
+feat_dim = spp_window_data_param.feat_dim;
 batch_per_file = spp_window_data_param.batch_per_file;
 batch_size = spp_window_data_param.batch_size;
 fg_fraction = spp_window_data_param.fg_fraction;
@@ -33,11 +33,13 @@ fg_overlap_max = spp_window_data_param.fg_overlap_max; % large than 1
 fg_overlap_min = spp_window_data_param.fg_overlap_min;
 bg_overlap_max = spp_window_data_param.bg_overlap_max;
 bg_overlap_min = spp_window_data_param.bg_overlap_min;
+extension = spp_window_data_param.extension;
+cache_dir = spp_window_data_param.cache_dir;
 
 load rcnn_model_spp.mat;
 roidb = imdb.roidb_func(imdb);
-if ~exist(out_dir, 'dir')
-  mkdir(out_dir);
+if ~exist(cache_dir, 'dir')
+  mkdir(cache_dir);
 end
 
 % initialize caffe
@@ -46,10 +48,9 @@ caffe('set_phase_test');
 
 fg_per_batch = round(batch_size * fg_fraction);
 bg_per_batch = batch_size - fg_per_batch;
-fg_limit = fg_per_batch * batch_per_file;
-bg_limit = bg_per_batch * batch_per_file;
 fg_cache = [];
 bg_cache = [];
+fg_label_cache = [];
 
 file_id = 0;
 for i = 1:length(imdb.image_ids)
@@ -63,47 +64,75 @@ for i = 1:length(imdb.image_ids)
   im = imread(img_path);
   % the roi.boxes are [x1 y1 x2 y2], 1-indexed
   feat = spp_features(im, roi.boxes, rcnn_model_spp);
-  fprintf('\n[Extracting feature: %f]\n', toc(th1));
+  fprintf('[Extracting feature: %f]\n', toc(th1));
   
   % put features into fg cache and bg cache
-  gt_overlap = max(roi.overlap, [], 2);
+  [gt_overlap, label] = max(roi.overlap, [], 2);
   is_fg = (gt_overlap >= fg_overlap_min) & (gt_overlap <= fg_overlap_max);
   is_bg = (gt_overlap >  bg_overlap_min) & (gt_overlap <  bg_overlap_max);
   fg_cache = cat(1, fg_cache, feat(is_fg, :));
   bg_cache = cat(1, bg_cache, feat(is_bg, :));
+  fg_label_cache = cat(1, fg_label_cache, label(is_fg));
   
-  % store features to disk
-  if size(fg_cache, 1) >= fg_limit && size(bg_cache, 1) >= bg_limit
+  % store features to disk, when
+  % case 1: the number of feature in a cache hits limit
+  % case 2: at the end there are still residue enough to build at least 1
+  % batch
+  fg_num = size(fg_cache, 1);
+  bg_num = size(bg_cache, 1);
+  has_residue_at_end = (i == length(imdb.image_ids)) ...
+      && (fg_num >= fg_per_batch) ...
+      && (bg_num >= bg_per_batch); 
+  hits_limit = (fg_num >= fg_per_batch * batch_per_file) ...
+      && (bg_num >= bg_per_batch * batch_per_file);
+  if has_residue_at_end || hits_limit
     th2 = tic();
     fprintf('------------------------------------------------------------\n');
-    fprintf('saving file id %d\n', file_id);
+    fprintf('saving file %d\n', file_id);
     fprintf('\t#fg in cache: %d\n', size(fg_cache, 1));
     fprintf('\t#bg in cache: %d\n', size(bg_cache, 1));
-    % assume that background windows are much more than foreground windows
-    assert(bg_num >= bg_limit);
-    % subsample background windows
-    bg_keep = sort(randsample(1:bg_num, bg_limit));
-    bg_cache = bg_cache(bg_keep, :);
     
-    % 12800-D spp5 feature is the fastest dimension
-    feat_batches = zeros(spp5_dim, batch_size, batch_per_file, 'single');
-    for b = 1:batch_per_file
+    % calculate the actual number of batches in this file
+    actual_batch_num = ...
+        min(floor(fg_num / fg_per_batch), floor(bg_num / bg_per_batch));
+    if i < length(imdb.image_ids)
+      actual_batch_num = min(actual_batch_num, batch_per_file);
+    end
+    if actual_batch_num ~= batch_per_file
+      fprintf('Notice: there are %d batches in this file', actual_batch_num);
+    end
+
+    % subsample background windows
+    if bg_num > bg_per_batch * actual_batch_num
+      bg_keep = randsample(1:bg_num, bg_per_batch * actual_batch_num);
+      bg_cache = bg_cache(bg_keep, :);
+    end
+
+    % write batches to disk
+    file_name = sprintf('%d%s', file_id, extension);
+    file_path = fullfile(cache_dir, file_name);
+    fid = fopen(file_path, 'w');
+    fwrite(fid, [actual_batch_num, batch_size, feat_dim], 'single');
+    for b = 1:actual_batch_num
       fg_cache_index = ((b-1)*fg_per_batch+1):(b*fg_per_batch);
       bg_cache_index = ((b-1)*bg_per_batch+1):(b*bg_per_batch);
-      feat_batches(:, 1:fg_per_batch, b) = ...
-          single(permute(fg_cache(fg_cache_index, :), [2, 1]));
-      feat_batches(:, (fg_per_batch+1):end, b) = ...
-          single(permute(bg_cache(bg_cache_index, :), [2, 1]));
+      % write labels (bg labels are zeros)
+      feat_labels = zeros(batch_size, 1, 'single');
+      feat_labels(1:fg_per_batch) = fg_label_cache(fg_cache_index);
+      fwrite(fid, feat_labels, 'single');
+
+      % write feature, 12800-D spp5 feature is the fastest dimension
+      feat_batch = zeros(feat_dim, batch_size, 'single');
+      feat_batch(:, 1:fg_per_batch) = ...
+        single(permute(fg_cache(fg_cache_index, :), [2, 1]));
+      feat_batch(:, (fg_per_batch+1):end) = ...
+        single(permute(bg_cache(bg_cache_index, :), [2, 1]));
+      fwrite(fid, feat_batch, 'single');
     end
-    % write batches to disk
-    file_name = sprintf('%d.spp_cache', file_id);
-    file_path = fullfile(out_dir, file_name);
-    fid = fopen(file_path, 'w');
-    fwrite(fid, feat_batches, 'single');
     fclose(fid);
-    
+
     % prune caches
-    fg_cache = fg_cache((fg_limit+1):end, :);
+    fg_cache = fg_cache((fg_per_batch * actual_batch_num+1):end, :);
     bg_cache = [];
     file_id = file_id + 1;
     fprintf('[Saving feature: %f]\n', toc(th2));
