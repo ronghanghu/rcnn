dd7be33a824d99da53a5c647b11b3b53e4dba84f
Ronghang Hu <huronghang@hotmail.com>
2014-08-11 09:11:18 -0700

    fix ilsvrc ft batch

--- experiments/rcnn_ilsvrc_exp_build_ft_batch.m
+++ experiments/rcnn_ilsvrc_exp_build_ft_batch.m
@@ -27,6 +27,7 @@ imdb_cell_train1k_val1 = cell(201, 1);
 inds_to_sample_cell_train1k_val1 = cell(201, 1);
 % add train_pos_1 to train_pos_200
 for i = 1:200
+    fprintf('loading training imdb %d/%d\n', i, 200);
     imdb_train = imdb_from_ilsvrc13(devkit, ['train_pos_' num2str(i)]);
     inds = find(imdb_train.is_blacklisted == false);
     num = min(length(inds), max_train_pos_num);
@@ -38,7 +39,7 @@ end
 imdb_val1 = imdb_from_ilsvrc13(devkit, 'val1');
 imdb_cell_train1k_val1{201} = imdb_val1;
 inds_to_sample_cell_train1k_val1{201} = find(imdb_val1.is_blacklisted == false);
-rcnn_pascal_make_spp_cache(imdb_cell_train1k_val1, ...
+rcnn_ilsvrc_make_spp_cache(imdb_cell_train1k_val1, ...
     inds_to_sample_cell_train1k_val1, split_num, ...
     net_proto_file, net_binary_file, cache_name, spp_feat_cache_param);
 
@@ -46,5 +47,5 @@ rcnn_pascal_make_spp_cache(imdb_cell_train1k_val1, ...
 spp_feat_cache_param.cache_dir = '/x/ronghang/ilsvrc13/val2';
 imdb_val2 = imdb_from_ilsvrc13(devkit, 'val2');
 inds_to_sample = find(imdb_val2.is_blacklisted == false);
-rcnn_pascal_make_spp_cache({imdb_val2}, {inds_to_sample}, split_num, ...
+rcnn_ilsvrc_make_spp_cache({imdb_val2}, {inds_to_sample}, split_num, ...
     net_proto_file, net_binary_file, cache_name, spp_feat_cache_param);
