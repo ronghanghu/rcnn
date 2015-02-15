2ce3fb1bed31017ec4bd6a1f7bd5141495e05b22
Ronghang Hu <huronghang@hotmail.com>
2015-02-15 02:20:58 -0800

    fix 10k_training files

--- 10k_training/make_10k_window_files.m
+++ 10k_training/make_10k_window_files.m
@@ -4,13 +4,13 @@ load 10k_training/map_vec_200_1k_to_7k.mat;
 
 imdb_ilsvrc13_det = [imdb_ilsvrc_val1 imdb_ilsvrc_val2 imdb_ilsvrc_train];
 imdb_ilsvrc12_loc = imdb;
-assert(strcmp(imdb_ilsvrc12_loc.name, 'imdb_ilsvrc12_loc_train'));
+assert(strcmp(imdb_ilsvrc12_loc.name, 'ilsvrc12_loc_train'));
 
 out_dir = '../external';
 
 num_to_sample = 1000;
 map_label = true(202, 1);
-whole_im = false;
+whole_im = false(202, 1);
 
 ending_index = rcnn_make_mapped_window_file(imdb_ilsvrc13_det, out_dir, 'mapped_200_to_7k', ...
     num_to_sample, map_label, whole_im, map_vec_200_to_7k, 0);
