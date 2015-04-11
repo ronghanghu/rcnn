fe65488026fdc880f9fb0b6f9a596f4ade8daba4
Ronghang Hu <huronghang@hotmail.com>
2015-04-11 14:37:24 +0800

    fix 1k ss

--- imagenet_10k/run_1k_selective_search.m
+++ imagenet_10k/run_1k_selective_search.m
@@ -1,5 +1,5 @@
 % 1k train
-load imdb/cache/imdb/cache/imdb_ilsvrc12_loc_train.mat
+load imdb/cache/imdb_ilsvrc12_loc_train.mat
 [images, boxes] = selective_search_boxes_imdb(imdb);
 fprintf('saving to file...\n');
 save_path = './data/selective_search_data';
@@ -8,7 +8,7 @@ fprintf('done\n');
 clear images boxes;
 
 % 1k val
-load imdb/cache/imdb/cache/imdb_ilsvrc12_loc_val.mat
+load imdb/cache/imdb_ilsvrc12_loc_val.mat
 [images, boxes] = selective_search_boxes_imdb(imdb);
 fprintf('saving to file...\n');
 save_path = './data/selective_search_data';
